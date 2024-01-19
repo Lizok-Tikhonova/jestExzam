ca8535261fa5c95c9d60ce333f3567d9822fc26e
Lizok-Tikhonova <97902948+Lizok-Tikhonova@users.noreply.github.com>
2024-01-19 20:47:44 +0300

    new version

--- 1.js
+++ 1.js
@@ -1,39 +1,44 @@
 
-function Perimetr(a,b){
-    let per = a + b + Math.sqrt(a**2+b**2)
-    return per 
-}
+     function Perimetr(a,b){
+          let per = a + b + Math.sqrt(a**2 + b**2)
+          if(a<=0 || b<=0){
+               return "Сторона не может быть отрицательной"
+          }
+          return per
+     }
+     function Squer(a,b){
+          let s = (a*b)
+          if(a<=0 || b<=0){
+               return "Сторона не может быть отрицательной"
+          }
+          return s
+     }
 
-function Square(a,b){
-    let sq = (a*b)*1/2
-    return sq 
-}
+     // let kat11 = prompt("введите первые два катета")
+     // let kat22 = prompt("введите вторые два катета")
 
-function main(){
-    let katets1 = prompt('Введите катеты первого треугольника, через пробел').split(' ')
-    if (katets1.length < 2 || katets1.length > 2){
-        alert('Некоректный ввод')
-        return 
-    }
-    let katets2 = prompt('Введите катеты первого треугольника, через пробел').split(' ')
-    if (katets1.length < 2 || katets1.length > 2){
-        alert('Некоректный ввод')
-        return 
-    }
-    let a = Number(katets1[0])
-    let b = Number(katets1[1])
-    let c = Number(katets2[0])
-    let d = Number(katets2[1])
-
-    let sumSquare = Square(a,b)+Square(c,d)
-    let sumPer = Perimetr(a,b)+Perimetr(c,d)
-
-    console.log(sumSquare, sumPer)
-
-    alert(`Сумма площадей равна ${sumSquare}`)
-    alert(`Сумма периметров равна ${sumPer}`)
-    return sumPer, sumSquare
-}
+    function Sum(kat1, kat2){
+          
+          kat1 = kat1.split(" ")
+          kat2 = kat2.split(" ")
+          console.log(kat1.length, kat2.length)
+          if(kat1.length != 2 || kat2.length != 2){
+               console.log('Вы ввели не два значения')
+               return 'Вы ввели не два значения'
+          }
+          if(Number.isNaN(Number(kat1[0])) || Number.isNaN(Number(kat1[1])) || Number.isNaN(Number(kat2[0])) || Number.isNaN(Number(kat2[1]))){
+               console.log('Вы ввели не числа')
+               return 'Вы ввели не числа'
+          }
+          
+          let sumPer = Perimetr(Number(kat1[0]),Number(kat1[1])) + Perimetr(Number(kat2[0]),Number(kat2[1]))
+          sumPer = sumPer.toFixed(2) 
+          return Number(sumPer)
+     }
+     // Sum(kat11, kat22)
 
 
-module.exports = {Perimetr, Square}
+     
+
+module.exports = {Perimetr, Squer, Sum}
+
